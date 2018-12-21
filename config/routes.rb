b73ca91c81fbeb70bcc3a2d4978827ec16685c06
john-raber <jrab224@gmail.com>
2018-12-21 12:57:20 -0500

    updating users/profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
+      resources :users, only: [:create] do
+        get 'profile', on: :collection
+      end
+
       post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
+      # get '/profile', to: 'users#profile'
     end
   end
 end
