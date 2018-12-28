105dd40f5e93188aac57c71c7893ba1944698be7
john-raber <jrab224@gmail.com>
2018-12-27 23:47:13 -0500

    removing pantheon relationship in god model; running migration for god and ability models

--- app/models/god.rb
+++ app/models/god.rb
@@ -1,4 +1,3 @@
 class God < ApplicationRecord
-  belongs_to :pantheon
   has_many :abilities
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_204623) do
+ActiveRecord::Schema.define(version: 2018_12_27_234459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abilities", force: :cascade do |t|
+    t.bigint "god_id"
+    t.string "cooldown"
+    t.string "cost"
+    t.text "description"
+    t.string "summary"
+    t.jsonb "menu_items"
+    t.jsonb "rank_items"
+    t.text "secondary_description"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["god_id"], name: "index_abilities_on_god_id"
+  end
+
+  create_table "gods", force: :cascade do |t|
+    t.decimal "attack_speed"
+    t.decimal "attack_speed_per_level"
+    t.string "cons"
+    t.decimal "hp5_per_level"
+    t.integer "health"
+    t.integer "health_per_five"
+    t.integer "health_per_level"
+    t.text "lore"
+    t.decimal "mp5_per_level"
+    t.integer "magic_protection"
+    t.decimal "magic_protection_per_level"
+    t.integer "magical_power"
+    t.decimal "magical_power_per_level"
+    t.integer "mana"
+    t.decimal "mana_per_five"
+    t.integer "mana_per_level"
+    t.string "name"
+    t.string "free_rotation"
+    t.string "pantheon"
+    t.integer "physical_power"
+    t.decimal "physical_power_per_level"
+    t.integer "physical_protection"
+    t.decimal "physical_protection_per_level"
+    t.string "pros"
+    t.string "roles"
+    t.integer "speed"
+    t.string "title"
+    t.string "type"
+    t.string "god_card_url"
+    t.string "god_icon_url"
+    t.string "latest_god"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -23,4 +74,5 @@ ActiveRecord::Schema.define(version: 2018_11_19_204623) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "abilities", "gods"
 end
